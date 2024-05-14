40b392b65f71bf698b78aec037563d4b757796d3
fronald143 <120772118+fronald143@users.noreply.github.com>
2024-05-14 18:25:37 +0800

    declare a bool

--- interpreter/Interpreter.java
+++ interpreter/Interpreter.java
@@ -156,7 +156,23 @@ class Interpreter implements Expr.Visitor<Object>,
         environment.define(stmt.name.lexeme, value, Tokentype);
         return null;
     }
-    
+
+    @Override
+    public Void visitBoolStmt(Bool stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+            if (!(value instanceof Boolean)) {
+                throw new RuntimeError(stmt.name, "Input must be an Boolean");
+            }
+        }
+
+        String Tokentype = "Boolean";
+
+        environment.define(stmt.name.lexeme, value, Tokentype);
+        return null;
+    }
+
     @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
